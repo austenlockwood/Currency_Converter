88b665f982ae7e0e9b703c8e1458d0d48926962a
Austen Lockwood <austenlockwood@gmail.com>
2016-06-24 16:24:08 -0400

    fixed error message to be appropriate for both addition and subtraction

--- Currency.rb
+++ Currency.rb
@@ -6,37 +6,37 @@ class Currency
   end                          # => :initialize
 
   def type
-    return @type  # => "SEA", "SEA", "WSK"
+    return @type  # => "SEA", "SEA", "WSK", "WSK", "SEA", "SEA"
   end             # => :type
 
   def value
-    return @value  # => 190, 190
+    return @value  # => 190, 190, 190
   end              # => :value
 
   def ==(other)
-    if @type == other.type && @value == other.value
+    if @type == other.type && @value == other.value  # => false, false
       return true #so if these instance variables match, it returns true instead of default false
     else
-      return false
+      return false                                   # => false, false
     end
   end                                                # => :==
 
   def +(other)
-    if @type == other.type                                                                         # => true, false
-      return @value + other.value                                                                  # => 200
+    if @type == other.type                                                                                   # => true, false
+      return @value + other.value                                                                            # => 200
     else
-      raise DifferentCurrencyCodeError, "You cannot add values between different currency types."  # ~> DifferentCurrencyCodeError: You cannot add values between different currency types.
+      raise DifferentCurrencyCodeError, "You cannot run this calculation between different currency types."
     end
-  end                                                                                              # => :+
+  end                                                                                                        # => :+
 
   def -(other)
-    if @type == other.type                                                                              # => true
-      return @value - other.value                                                                       # => -180
+    if @type == other.type                                                                                   # => true, false
+      return @value - other.value                                                                            # => -180
     else
-      raise DifferentCurrencyCodeError, "You cannot subtract values between different currency types."
+      raise DifferentCurrencyCodeError, "You cannot run this calculation between different currency types."
     end
-  end                                                                                                   # => :-
-end                                                                                                     # => :-
+  end                                                                                                        # => :-
+end                                                                                                          # => :-
 
 class DifferentCurrencyCodeError < StandardError  # => StandardError
 end                                               # => nil
@@ -44,23 +44,18 @@ end                                               # => nil
 
 
 
-seashell = Currency.new("SEA", 10)        # => #<Currency:0x007fcb7501aa98 @type="SEA", @value=10>
-seashell2 = Currency.new("SEA", 190)      # => #<Currency:0x007fcb7501a278 @type="SEA", @value=190>
-whiskey_bottle = Currency.new("WSK", 10)  # => #<Currency:0x007fcb75019b98 @type="WSK", @value=10>
+seashell = Currency.new("SEA", 10)        # => #<Currency:0x007fa03a0162b0 @type="SEA", @value=10>
+seashell2 = Currency.new("SEA", 190)      # => #<Currency:0x007fa03a015b08 @type="SEA", @value=190>
+whiskey_bottle = Currency.new("WSK", 10)  # => #<Currency:0x007fa03a015478 @type="WSK", @value=10>
 
-test_value = seashell.+(seashell2)        # => 200
-test_sub = seashell.-(seashell2)          # => -180
+test_value = seashell.+(seashell2)          # => 200
+test_sub = seashell.-(seashell2)            # => -180
+
 test_clash = seashell.+(whiskey_bottle)
 test_clash2 = seashell.-(whiskey_bottle)
-
-seashell.==(seashell2)
-whiskey_bottle.==(seashell)
+                                      # => nil
+seashell.==(seashell2)                      # => false
+whiskey_bottle.==(seashell)                 # => false
 
 
 # need a method that will compare an instance variable not the entire shebang
-
-# ~> DifferentCurrencyCodeError
-# ~> You cannot add values between different currency types.
-# ~>
-# ~> /Users/austenlockwood/Desktop/TIYJun/Wk01/currency_converter/Currency.rb:28:in `+'
-# ~> /Users/austenlockwood/Desktop/TIYJun/Wk01/currency_converter/Currency.rb:53:in `<main>'
