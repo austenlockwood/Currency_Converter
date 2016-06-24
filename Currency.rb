4241e3498b763958e04e88bb9f3f973b927d5605
Austen Lockwood <austenlockwood@gmail.com>
2016-06-24 16:06:22 -0400

    cannot add or subtract between different currency codes.  Error message pops up.

--- Currency.rb
+++ Currency.rb
@@ -1,35 +1,66 @@
 class Currency
 
-  def initialize(type, denom)
+  def initialize(type, value)
     @type = type               # => "SEA", "SEA", "WSK"
-    @denom = denom             # => 10, 10, 10
+    @value = value             # => 10, 190, 10
   end                          # => :initialize
 
   def type
-    return @type  # => "SEA", "SEA"
+    return @type  # => "SEA", "SEA", "WSK"
   end             # => :type
 
-  def denom
-    return @denom  # => 10
-  end              # => :denom
+  def value
+    return @value  # => 190, 190
+  end              # => :value
 
   def ==(other)
-    if @type == other.type && @denom == other.denom  # => true, false
+    if @type == other.type && @value == other.value
       return true #so if these instance variables match, it returns true instead of default false
     else
-      return false                                   # => false
+      return false
     end
   end                                                # => :==
 
-end  # => :==
+  def +(other)
+    if @type == other.type                                                                         # => true, false
+      return @value + other.value                                                                  # => 200
+    else
+      raise DifferentCurrencyCodeError, "You cannot add values between different currency types."  # ~> DifferentCurrencyCodeError: You cannot add values between different currency types.
+    end
+  end                                                                                              # => :+
 
-seashell = Currency.new("SEA", 10)        # => #<Currency:0x007f8a0a81ede0 @type="SEA", @denom=10>
-seashell2 = Currency.new("SEA", 10)       # => #<Currency:0x007f8a0a81d170 @type="SEA", @denom=10>
-whiskey_bottle = Currency.new("WSK", 10)  # => #<Currency:0x007f8a0a81c928 @type="WSK", @denom=10>
+  def -(other)
+    if @type == other.type                                                                              # => true
+      return @value - other.value                                                                       # => -180
+    else
+      raise DifferentCurrencyCodeError, "You cannot subtract values between different currency types."
+    end
+  end                                                                                                   # => :-
+end                                                                                                     # => :-
 
-seashell.==(seashell2)       # => true
-whiskey_bottle.==(seashell)  # => false
+class DifferentCurrencyCodeError < StandardError  # => StandardError
+end                                               # => nil
 
-# seashell.denom         # ~> NoMethodError: undefined method `denom' for #<Currency:0x007f878b81bb80 @type="FRC", @denom=0.56>
+
+
+
+seashell = Currency.new("SEA", 10)        # => #<Currency:0x007fcb7501aa98 @type="SEA", @value=10>
+seashell2 = Currency.new("SEA", 190)      # => #<Currency:0x007fcb7501a278 @type="SEA", @value=190>
+whiskey_bottle = Currency.new("WSK", 10)  # => #<Currency:0x007fcb75019b98 @type="WSK", @value=10>
+
+test_value = seashell.+(seashell2)        # => 200
+test_sub = seashell.-(seashell2)          # => -180
+test_clash = seashell.+(whiskey_bottle)
+test_clash2 = seashell.-(whiskey_bottle)
+
+seashell.==(seashell2)
+whiskey_bottle.==(seashell)
+
 
 # need a method that will compare an instance variable not the entire shebang
+
+# ~> DifferentCurrencyCodeError
+# ~> You cannot add values between different currency types.
+# ~>
+# ~> /Users/austenlockwood/Desktop/TIYJun/Wk01/currency_converter/Currency.rb:28:in `+'
+# ~> /Users/austenlockwood/Desktop/TIYJun/Wk01/currency_converter/Currency.rb:53:in `<main>'
