90ed5974c4ecfb0b8c37b77ff9ddca212c27de71
Austen Lockwood <austenlockwood@gmail.com>
2016-06-24 14:49:47 -0400

    Currency objects are able to: 1. be created with an amount and currency code; 2. equal another currency object with the same amount and currency code; and 3. return false when compared to another currency object with either a different currency code or a different amount.

--- Currency.rb
+++ Currency.rb
@@ -1,32 +1,35 @@
 class Currency
 
-  def initialize(type, rate)
-    @type = type
-    @rate = rate
-  end
+  def initialize(type, denom)
+    @type = type               # => "SEA", "SEA", "WSK"
+    @denom = denom             # => 10, 10, 10
+  end                          # => :initialize
 
   def type
-    return @type
-  end
+    return @type  # => "SEA", "SEA"
+  end             # => :type
 
-  def rate
-    return @rate
-  end
+  def denom
+    return @denom  # => 10
+  end              # => :denom
 
   def ==(other)
-    if @type == other.type && @rate == other.rate
+    if @type == other.type && @denom == other.denom  # => true, false
       return true #so if these instance variables match, it returns true instead of default false
     else
-      return false
+      return false                                   # => false
     end
-  end
+  end                                                # => :==
 
-end
+end  # => :==
 
-francs = Currency.new("FRC", 0.56)
-francs2 = Currency.new("FRC", 0.56)
+seashell = Currency.new("SEA", 10)        # => #<Currency:0x007f8a0a81ede0 @type="SEA", @denom=10>
+seashell2 = Currency.new("SEA", 10)       # => #<Currency:0x007f8a0a81d170 @type="SEA", @denom=10>
+whiskey_bottle = Currency.new("WSK", 10)  # => #<Currency:0x007f8a0a81c928 @type="WSK", @denom=10>
 
-francs.==(francs2)
-# francs.rate         # ~> NoMethodError: undefined method `rate' for #<Currency:0x007f878b81bb80 @type="FRC", @rate=0.56>
+seashell.==(seashell2)       # => true
+whiskey_bottle.==(seashell)  # => false
+
+# seashell.denom         # ~> NoMethodError: undefined method `denom' for #<Currency:0x007f878b81bb80 @type="FRC", @denom=0.56>
 
 # need a method that will compare an instance variable not the entire shebang
