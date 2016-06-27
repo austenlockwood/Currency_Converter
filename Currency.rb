bd8cb448e3f729175ebccacc99058dce98f34bfe
Austen Lockwood <austenlockwood@gmail.com>
2016-06-27 08:04:32 -0400

    edited initialize block so it will accept currency symbols (£) as well as three letter code (GBP) as a type.  However, now have an error message about undefined method for type when running .- and .+ methods.

--- Currency.rb
+++ Currency.rb
@@ -1,21 +1,39 @@
+# encoding: UTF-8
 class Currency
 
+@@currency_symbols = {
+  "$" => "USD",
+  "€" => "EUR",
+  "¥" => "CNY",
+  "£" => "GBP",
+  "₪" => "ILS",
+  "₹" => "INR",
+  "₩" => "KRW",
+  "₽" => "RUB",
+  "₴" => "UAH"
+}
+
   def initialize(type, value)
-    @type = type
-    @value = value
-  end
+    if @@currency_symbols.has_value?(type)
+      @type = type
+    elsif @@currency_symbols.has_key?(type) #if the class hash has what you wrote for type as a key, then
+      @type = @@currency_symbols.fetch(type) #you will reassign the associated value as the value for @type
+    else
+      raise UnrecognizedCurrencyError, "This is not a recognized currency symbol."
+    end
 
-  def type
-    return @type
-  end
-
-  def value
-    return @value
+    if value.is_a? Float
+      @value = value
+    elsif value.is_a? Fixnum
+      @value = value.to_f
+    else
+      raise BadFactorError, "This is not a valid factor."
+    end
   end
 
   def ==(other)
     if @type == other.type && @value == other.value
-      return true #so if these instance variables match, it returns true instead of default false
+      return true
     else
       return false
     end
@@ -50,24 +68,19 @@ class Currency
 
 end
 
-class DifferentCurrencyCodeError < StandardError
-end
+class DifferentCurrencyCodeError < StandardError; end
+class BadFactorError < StandardError; end
+class UnrecognizedCurrencyError < StandardError; end
 
-class BadFactorError < StandardError
-end
+seashell = Currency.new("£", 10)
+seashell2 = Currency.new("GBP", 190.0)
+# whiskey_bottle = Currency.new("USD", 10)
 
-seashell = Currency.new("SEA", 10.9)
-seashell2 = Currency.new("SEA", 190)
-whiskey_bottle = Currency.new("WSK", 10)
-#
 # test_value = seashell.+(seashell2)
-# test_sub = seashell.-(seashell2)
-test_multiply = seashell.*(4.3)
+test_sub = seashell.-(seashell2)
+test_multiply = seashell.*(11)
 
 # test_clash = seashell.+(whiskey_bottle)
 # test_clash2 = seashell.-(whiskey_bottle)
 # seashell.==(seashell2)
 # whiskey_bottle.==(seashell)
-
-
-# need a method that will compare an instance variable not the entire shebang
